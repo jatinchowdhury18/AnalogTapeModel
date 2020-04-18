21fbec6a760304fd24aa075459743427944e28e5
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2020-04-18 15:50:20 -0700

    Revert "First attempt at pre/post EQ"
    This reverts commit 3ae9c9756af145f772c33e7630a24d815617ed4b.

--- Plugin/Source/GUI Components/MainControls.cpp
+++ Plugin/Source/GUI Components/MainControls.cpp
@@ -7,9 +7,6 @@ MainControls::MainControls (ChowtapeModelAudioProcessor& proc) :
     ChowtapeModelAudioProcessorEditor::createSlider (gainInKnob,  proc.getVTS(), "ingain",  gainInAttach,  *this, myLNF, " dB");
     ChowtapeModelAudioProcessorEditor::createSlider (gainOutKnob, proc.getVTS(), "outgain", gainOutAttach, *this, myLNF, " dB");
     ChowtapeModelAudioProcessorEditor::createComboBox (oversampling, proc.getVTS(), "os", osAttach, this, StringArray ({"2x", "4x", "8x", "16x"}));
-    ChowtapeModelAudioProcessorEditor::createButton (prePostEQButton, proc.getVTS(), "prepost", prePostAttach, this, "ON/OFF",
-                                                     Colours::darkred.withMultipliedBrightness (1.2f));
-
     // ChowtapeModelAudioProcessorEditor::createComboBox (oversampling, processor.overSampling, this);
     // ChowtapeModelAudioProcessorEditor::createComboBox (tapeSpeed, processor.tapeSpeed, this);
     // //ChowtapeModelAudioProcessorEditor::createComboBox (tapeType, processor.tapeType, this);
@@ -18,8 +15,6 @@ MainControls::MainControls (ChowtapeModelAudioProcessor& proc) :
     ChowtapeModelAudioProcessorEditor::createLabel (inGainLabel, gainInKnob.getName(), this);
     ChowtapeModelAudioProcessorEditor::createLabel (outGainLabel, gainOutKnob.getName(), this);
     ChowtapeModelAudioProcessorEditor::createLabel (oversampleLabel, oversampling.getName(), this);
-    ChowtapeModelAudioProcessorEditor::createLabel (prePostEQLabel, prePostEQButton.getName(), this);
-
     // ChowtapeModelAudioProcessorEditor::createLabel (speedLabel, processor.tapeSpeed, this);
     // //ChowtapeModelAudioProcessorEditor::createLabel (typeLabel, processor.tapeType, this);
 }
@@ -33,9 +28,6 @@ void MainControls::resized()
 
     oversampleLabel.setBounds (gainInKnob.getRight() - 7 * xOffset, 2 * labelY + yOffset, tapeWidth, labelHeight);
     oversampling.setBounds (gainInKnob.getRight(), oversampleLabel.getBottom(), overWidth, boxHeight);
-
-    prePostEQLabel.setBounds (oversampleLabel.getX(), oversampling.getBottom() + 5, tapeWidth, labelHeight);
-    prePostEQButton.setBounds (prePostEQLabel.getX() + 15, prePostEQLabel.getBottom() + 3, overWidth * 4 / 5, boxHeight);
 
     // speedLabel.setBounds (oversampling.getRight(), 2 * labelY + yOffset, tapeWidth, labelHeight);
     // tapeSpeed.setBounds (oversampling.getRight() + 2 * xOffset, speedLabel.getBottom(), tapeWidth, boxHeight);
