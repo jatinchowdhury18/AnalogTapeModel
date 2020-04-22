ccbe0b227a5cd0153efa1fc4197d5432b68dc9c4
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2020-04-21 19:44:10 -0700

    Comment chewer and degrade processing

--- Plugin/Source/PluginProcessor.cpp
+++ Plugin/Source/PluginProcessor.cpp
@@ -171,8 +171,8 @@ void ChowtapeModelAudioProcessor::processBlock (AudioBuffer<float>& buffer, Midi
 
     inGain.processBlock (buffer, midiMessages);
     hysteresis.processBlock (buffer, midiMessages);
-    chewer.processBlock (buffer);
-    degrade.processBlock (buffer, midiMessages);
+    // chewer.processBlock (buffer);
+    // degrade.processBlock (buffer, midiMessages);
     
     flutter.processBlock (buffer, midiMessages);
 
