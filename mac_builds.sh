02426add7f64defe24d37f9a912a6f797dcb9968
Travis CI <travis@Traviss-Mac-6.local>
2020-08-13 03:25:24 +0000

    New Mac builds

---
+++ mac_builds.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+travis_run_before_install
+travis_run_script
+
+cd $TRAVIS_BUILD_DIR/Plugin/Bin/Mac
+rm -Rf CHOWTapeModel.*
+
+cp -Rf $TRAVIS_BUILD_DIR/Plugin/Builds/MacOSX/build/Release/CHOWTapeModel.app CHOWTapeModel.app
+cp -Rf ~/Library/Audio/Plug-Ins/VST3/CHOWTapeModel.vst3 CHOWTapeModel.vst3
+cp -Rf ~/Library/Audio/Plug-Ins/VST/CHOWTapeModel.vst CHOWTapeModel.vst
+cp -Rf ~/Library/Audio/Plug-Ins/Components/CHOWTapeModel.component CHOWTapeModel.component
+
