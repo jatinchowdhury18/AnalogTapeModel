b9f069b6e70236a5d40b7e18cf4da14e07dc662f
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2020-08-23 11:27:30 -0700

    Add simple auto-update checking (#74)
    * Add simple auto-update checking
    
    * Update changelog
    
    Co-authored-by: jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>

---
+++ Plugin/Source/GUI/AutoUpdating.h
@@ -0,0 +1,55 @@
+#ifndef AUTOUPDATING_H_INCLUDED
+#define AUTOUPDATING_H_INCLUDED
+
+#include <JuceHeader.h>
+#include "MyLNF.h"
+
+struct UpdateButtonLNF : public MyLNF
+{
+    UpdateButtonLNF() {}
+
+    Font getTextButtonFont (TextButton&, int /*buttonHeight*/) override
+    {
+        return Font (30.0f).boldened();
+    }
+
+    void drawButtonBackground (Graphics& g, Button& button, const Colour& backgroundColour, bool, bool) override
+    {
+        LookAndFeel_V4::drawButtonBackground (g, button, backgroundColour, false, false);
+    }
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (UpdateButtonLNF)
+};
+
+class AutoUpdater : public Component
+{
+public:
+    AutoUpdater();
+    ~AutoUpdater();
+
+    void paint (Graphics& g) override;
+    void resized() override;
+
+    bool runAutoUpdateCheck();
+    void noButtonPressed();
+    void yesButtonPressed();
+
+private:
+    File getUpdateCheckFile();
+    String getLatestVersion();
+    String getUpdateFileVersion (const File& updateFile);
+    bool getUpdateFileYesNo (const File& updateFile);
+    void editUpdateCheckFile (String version, bool wantsUpdate);
+
+    String newVersion = String (JucePlugin_VersionString);
+
+    TextButton yesButton { "Yes" };
+    TextButton noButton { "No" };
+    UpdateButtonLNF ubLNF;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AutoUpdater)
+};
+
+
+#endif // AUTOUPDATING_H_INCLUDED
