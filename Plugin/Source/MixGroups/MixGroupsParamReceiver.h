4c1bfb2ee691d8d713321518168c13a628b81292
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2020-09-14 12:10:49 -0700

    Manage mix groups through shared resource pointer, instead of OSC (#88)
    * Manage mix groups through shared resource pointer, instead of OSC
    
    * Update Mac builds
    
    * Update Windows builds
    
    Co-authored-by: jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
    Co-authored-by: Travis CI <travis@Traviss-Mac-6.local>

--- Plugin/Source/MixGroups/MixGroupsParamReceiver.h
+++ Plugin/Source/MixGroups/MixGroupsParamReceiver.h
@@ -3,27 +3,44 @@
 
 #include <JuceHeader.h>
 
-/** 
- * Class to receive parameter info from other instances of the plugin.
- * Create instances of this class with a SharedResourcePointer, otherwise
- * the messages won't sync correctly between instances of the plugin.
- */
-class MixGroupsParamReceiver : public ChangeBroadcaster,
-                               private OSCReceiver,
-                               private OSCReceiver::ListenerWithOSCAddress<OSCReceiver::MessageLoopCallback>
+class MixGroupsSharedData
 {
 public:
-    MixGroupsParamReceiver();
+    MixGroupsSharedData();
 
-    void oscMessageReceived (const OSCMessage& message) override;
-    void loadReceiverListeners (Array<String>& paramList);
-    const OSCMessage& getOSCMessage() { return oscMessage; }
+    /** Create parameter maps from list of parameters */
+    void loadParameterList (Array<String>& paramList);
+
+    /** A plugin has changed mix group. */
+    void pluginGroupChanged (const String& pluginID, int mixGroup);
+
+    /** Get the number of plugins already in this mix group */
+    int getNumPluginsInGroup (int mixGroup) const;
+
+    /** Copy the plugin state into the parameter map for a given mix group */
+    void copyPluginState (int mixGroup, AudioProcessorValueTreeState& vts);
+
+    void setParameter (const String& paramID, int mixGroup, float value, String uuid);
+    float getParameter (const String& paramID, int mixGroup) const;
+
+    struct Listener
+    {
+        ~Listener() {}
+        virtual void mixGroupParamChanged (const String& /*paramID*/, int /*mixGroup*/, float /*value*/, String /*uuid*/) {}
+    };
+
+    void addListener (Listener* l) { listeners.add (l); }
+    void removeListener (Listener* l) { listeners.remove (l); }
 
 private:
-    DatagramSocket socket;
-    OSCMessage      oscMessage { "/noaddress" };
-    bool            listenersInitialized = false;
+    using ParamMap = HashMap<String, float>;
+    std::vector<std::unique_ptr<ParamMap>> paramMaps;
+
+    OwnedArray<StringArray> pluginsInGroup; // list of plugin IDs of all the plugins in each mix group
+
+    ListenerList<Listener> listeners;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MixGroupsSharedData)
 };
 
 #endif // MIXGROUPSPARAMRECEIVER
-
