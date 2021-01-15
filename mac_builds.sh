766da042ab1e56f816144f397071833c51cc4564
Jatin Chowdhury <jachowdhury@tesla.com>
2021-01-14 21:08:44 -0800

    Update builds and CI

--- mac_builds.sh
+++ mac_builds.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
-travis_run_before_install
-travis_run_script
+# exit on failure
+set -e
 
-cd $TRAVIS_BUILD_DIR/Plugin/Bin/Mac
-rm -Rf CHOWTapeModel.*
+# clean up old builds
+cd Plugin
+rm -Rf build/
+rm -Rf Bin/*Mac*
 
-cp -Rf $TRAVIS_BUILD_DIR/Plugin/Builds/MacOSX/build/Release/CHOWTapeModel.app CHOWTapeModel.app
-cp -Rf ~/Library/Audio/Plug-Ins/VST3/CHOWTapeModel.vst3 CHOWTapeModel.vst3
-cp -Rf ~/Library/Audio/Plug-Ins/VST/CHOWTapeModel.vst CHOWTapeModel.vst
-cp -Rf ~/Library/Audio/Plug-Ins/Components/CHOWTapeModel.component CHOWTapeModel.component
+# set up build VST
+VST_PATH=~/Developer/VST2_SDK/
+sed -i '' "56s~.*~juce_set_vst2_sdk_path(${VST_PATH})~" CMakeLists.txt
+sed -i '' '63s/#//' CMakeLists.txt
 
-cd ../
-zip -r MacBuilds.zip Mac
+# cmake new builds
+TEAM_ID=$(more ~/Developer/mac_id)
+cmake -Bbuild -GXcode -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY="Apple Distribution" \
+    -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=$TEAM_ID \
+    -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_STYLE="Manual" \
+    -D"CMAKE_OSX_ARCHITECTURES=arm64;x86_64"
+cmake --build build --config Release -j8 | xcpretty
+
+# copy builds to Bin
+mkdir -p Bin/Mac
+declare -a plugins=("CHOWTapeModel")
+for plugin in "${plugins[@]}"; do
+    cp -R build/${plugin}_artefacts/Release/Standalone/${plugin}.app Bin/Mac/${plugin}.app
+    cp -R build/${plugin}_artefacts/Release/VST/${plugin}.vst Bin/Mac/${plugin}.vst
+    cp -R build/${plugin}_artefacts/Release/VST3/${plugin}.vst3 Bin/Mac/${plugin}.vst3
+    cp -R build/${plugin}_artefacts/Release/AU/${plugin}.component Bin/Mac/${plugin}.component
+done
+
+# reset CMakeLists.txt
+git restore CMakeLists.txt
+
+# zip builds
+VERSION=$(cut -f 2 -d '=' <<< "$(grep 'CMAKE_PROJECT_VERSION:STATIC' build/CMakeCache.txt)")
+(
+    cd bin
+    rm -f "CHOWTapeModel-Mac-${VERSION}.zip"
+    zip -r "CHOWTapeModel-Mac-${VERSION}.zip" Mac
+)
