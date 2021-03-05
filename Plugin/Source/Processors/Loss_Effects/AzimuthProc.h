7533091a6558a49e6d347eb9b4ec01e607505f21
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2021-03-05 15:14:03 -0800

    Add parameter to adjust playhead azimuth. (#149)
    * Python sims
    
    * Set up placeholder code for Azimuth
    
    * Add azimuth processing
    
    * {Apply clang-format}
    
    Co-authored-by: jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ Plugin/Source/Processors/Loss_Effects/AzimuthProc.h
@@ -0,0 +1,25 @@
+#ifndef AZIMUTHPROC_H_INCLUDED
+#define AZIMUTHPROC_H_INCLUDED
+
+#include <JuceHeader.h>
+
+class AzimuthProc
+{
+public:
+    AzimuthProc() = default;
+
+    void prepare (double sampleRate, int samplesPerBlock);
+    void setAzimuthAngle (float angleDeg, float tapeSpeedIps);
+    void processBlock (AudioBuffer<float>& buffer);
+
+private:
+    using ADelayLine = dsp::DelayLine<float, dsp::DelayLineInterpolationTypes::Lagrange3rd>;
+    std::unique_ptr<ADelayLine> delays[2];
+    SmoothedValue<float, ValueSmoothingTypes::Linear> delaySampSmooth[2];
+
+    float fs = 48000.0f;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AzimuthProc)
+};
+
+#endif // AZIMUTHPROC_H_INCLUDED
