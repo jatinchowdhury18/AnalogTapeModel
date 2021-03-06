42aad7adda8c2a8f333ae5f51b3e1d9c68f5c6a1
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-03-06 05:36:17 -0800

    More warning flag adjustments

--- Plugin/modules/WarningFlags.cmake
+++ Plugin/modules/WarningFlags.cmake
@@ -5,7 +5,7 @@ if((CMAKE_CXX_COMPILER_ID STREQUAL "MSVC") OR (CMAKE_CXX_SIMULATE_ID STREQUAL "M
         /W4     # base warning level
         /wd4458 # declaration hides class member (from Foley's GUI Magic)
         /wd4505 # since VS2019 doesn't handle [[ maybe_unused ]] for static functions (RTNeural::debug_print)
-        /EHsc   # Enable unwind semantics for exception handler (nlohmann::json)
+        /wd4530 # Unwind semantics for exception handler (nlohmann::json)
     )
 elseif((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") OR (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang"))
     target_compile_options(warning_flags INTERFACE
@@ -17,20 +17,25 @@ elseif((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") OR (CMAKE_CXX_COMPILER_ID STREQU
         -Wzero-as-null-pointer-constant -Wcast-align
         -Wno-inconsistent-missing-destructor-override -Wshift-sign-overflow
         -Wnullable-to-nonnull-conversion -Wno-missing-field-initializers
-        -Wno-ignored-qualifiers -Wno-switch-enum -Wpedantic -Wno-pessimizing-move
+        -Wno-ignored-qualifiers -Wpedantic -Wno-pessimizing-move
         # These lines suppress some warnings from Foley's GUI Magic and RTNeural.
         # Comment them out to be more strict.
         -Wno-shadow-field-in-constructor -Wno-shadow-field -Wno-sign-conversion
-        -Wno-float-conversion -Wno-implicit-float-conversion
+        -Wno-float-conversion -Wno-implicit-float-conversion -Wno-switch-enum
     )
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     target_compile_options(warning_flags INTERFACE
         -Wall -Wextra -Wstrict-aliasing -Wuninitialized -Wunused-parameter
         -Wsign-compare -Woverloaded-virtual -Wreorder -Wunreachable-code
         -Wzero-as-null-pointer-constant -Wcast-align -Wno-implicit-fallthrough
-        -Wno-maybe-uninitialized -Wno-missing-field-initializers
-        -Wno-ignored-qualifiers -Wno-switch-enum -Wredundant-decls
-        -Wno-pedantic -Wno-sign-conversion -Wno-unused-function
+        -Wno-maybe-uninitialized -Wno-missing-field-initializers -Wno-pedantic
+        -Wno-ignored-qualifiers -Wredundant-decls -Wno-unused-function
+        -Wno-pessimizing-move -Wno-overloaded-virtual
+        # These lines suppress some warnings from Foley's GUI Magic and RTNeural.
+        # Comment them out to be more strict.
+        -Wno-sign-conversion -Wno-switch-enum
+        # From LV2 Wrapper
+        -Wno-parentheses -Wno-deprecated-declarations
     )
 
     if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER "7.0.0")
