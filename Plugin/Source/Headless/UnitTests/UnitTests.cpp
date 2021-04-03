c3177ae5e2c0a65c2b7327a8c4d86d489c3e2f6d
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2021-04-03 14:32:50 -0400

    Set up unit tests (#176)
    * Set up unit test framework
    
    * Disable ScreenshotHelper for Linux
    
    * Fix Linux headless compilation
    
    * {Apply clang-format}
    
    * Implement MixGroupsTest
    
    * {Apply clang-format}
    
    * Implement SpeedTest
    
    Co-authored-by: jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ Plugin/Source/Headless/UnitTests/UnitTests.cpp
@@ -0,0 +1,79 @@
+#include "UnitTests.h"
+
+//==============================================================================
+class ConsoleLogger : public Logger
+{
+    void logMessage (const String& message) override
+    {
+        std::cout << message << std::endl;
+
+#if JUCE_WINDOWS
+        Logger::outputDebugString (message);
+#endif
+    }
+};
+
+//==============================================================================
+class ConsoleUnitTestRunner : public UnitTestRunner
+{
+    void logMessage (const String& message) override
+    {
+        Logger::writeToLog (message);
+    }
+};
+
+//==============================================================================
+UnitTests::UnitTests()
+{
+    this->commandOption = "--unit-tests";
+    this->argumentDescription = "--unit-tests [--seed=RANDOM_SEED --all] TEST1 TEST2 ...";
+    this->shortDescription = "Runs unit tests for ChowTapeModel";
+    this->longDescription = "";
+    this->command = std::bind (&UnitTests::runUnitTests, this, std::placeholders::_1);
+}
+
+void UnitTests::runUnitTests (const ArgumentList& args)
+{
+    ConsoleLogger logger;
+    Logger::setCurrentLogger (&logger);
+
+    ConsoleUnitTestRunner runner;
+    auto seed = getRandomSeed (args);
+
+    auto tests = UnitTest::getAllTests();
+    getTestsForArgs (tests, args);
+
+    runner.runTests (tests, seed);
+
+    Logger::setCurrentLogger (nullptr);
+
+    for (int i = 0; i < runner.getNumResults(); ++i)
+        if (runner.getResult (i)->failures > 0)
+            ConsoleApplication::fail ("Unit Tests failed!");
+}
+
+int64 UnitTests::getRandomSeed (const ArgumentList& args)
+{
+    if (args.containsOption ("--seed"))
+    {
+        auto seedValueString = args.getValueForOption ("--seed");
+        if (seedValueString.startsWith ("0x"))
+            return seedValueString.getHexValue64();
+
+        return seedValueString.getLargeIntValue();
+    }
+
+    return int64 (0);
+}
+
+void UnitTests::getTestsForArgs (Array<UnitTest*>& tests, const ArgumentList& args)
+{
+    if (args.containsOption ("--all"))
+        return;
+
+    for (auto* t : tests)
+    {
+        if (! args.containsOption (t->getName()))
+            tests.remove (&t);
+    }
+}
