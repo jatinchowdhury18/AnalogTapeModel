9e8dbc4aef83e0485dea60d07b6e8b15e25a479e
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-04-03 13:45:17 -0400

    Implement MixGroupsTest

--- Plugin/Source/Headless/UnitTests/MixGroupsTest.cpp
+++ Plugin/Source/Headless/UnitTests/MixGroupsTest.cpp
@@ -1,18 +1,87 @@
-#include "../../PluginProcessor.h"
+#include "PluginProcessor.h"
+#include "MixGroups/MixGroupsController.h"
 
 class MixGroupsTest : public UnitTest
 {
+    using Proc = ChowtapeModelAudioProcessor;
 public:
     MixGroupsTest() : UnitTest ("MixGroupsTest")
     {
     }
 
+    void setMixGroup (Proc* plugin, float value)
+    {
+        for (auto* p : plugin->getParameters())
+        {
+            if (auto* pCast = dynamic_cast<RangedAudioParameter*> (p))
+            {
+                if (pCast->paramID == MixGroupsConstants::mixGroupParamID)
+                    pCast->setValueNotifyingHost (value);
+            }
+        }
+    }
+
+    std::unique_ptr<Proc> createPlugin()
+    {
+        auto proc = createPluginFilterOfType (AudioProcessor::WrapperType::wrapperType_Standalone);
+        std::unique_ptr<Proc> plugin (dynamic_cast<Proc*> (proc));
+        return std::move (plugin);
+    }
+
+    void compareStates (Proc* plugin1, Proc* plugin2)
+    {
+        for (int i = 0; i < plugin1->getParameters().size(); ++i)
+        {
+            auto* param1 = dynamic_cast<RangedAudioParameter*> (plugin1->getParameters()[i]);
+            auto* param2 = dynamic_cast<RangedAudioParameter*> (plugin2->getParameters()[i]);
+
+            if (! param1 || ! param2)
+                continue;
+
+            expectWithinAbsoluteError (param1->getValue(), param2->getValue(), 0.001f,
+                "Parameter " + param1->name + " is not linked correctly!");
+        }
+    }
+
+    void copyStateTest()
+    {
+        auto plugin1 = createPlugin();
+        auto plugin2 = createPlugin();
+
+        plugin1->setCurrentProgram (4);
+        setMixGroup (plugin1.get(), 1.0f);
+        setMixGroup (plugin2.get(), 1.0f);
+
+        compareStates (plugin1.get(), plugin2.get());
+    }
+
+    void parameterLinkTest()
+    {
+        auto plugin1 = createPlugin();
+        auto plugin2 = createPlugin();
+
+        setMixGroup (plugin1.get(), 1.0f);
+        setMixGroup (plugin2.get(), 1.0f);
+
+        auto params1 = plugin1->getParameters();
+        auto params2 = plugin2->getParameters();
+
+        constexpr int numRuns = 100;
+        for (int i = 0; i < numRuns; ++i)
+        {
+            auto paramIdxToChange = getRandom().nextInt (params1.size());
+            params1[paramIdxToChange]->setValueNotifyingHost (getRandom().nextFloat());
+            compareStates (plugin1.get(), plugin2.get());
+        }
+    }
+
     void runTest() override
     {
-        beginTest ("Dummy Test");
-        std::cout << "Testing..." << std::endl;
+        beginTest ("Copy State Test");
+        copyStateTest();
 
-        expect (false, "This SHOULD fail!");
+        beginTest ("Parameter Link Test");
+        parameterLinkTest();
     }
 };
 
