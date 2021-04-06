0d8a1c315d07f0385adce5c28a0a68350bcf3f9f
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2021-04-06 16:54:55 -0400

    Update RTNeural to make sure it uses the correct backend (#179)
    * Update RTNeural to make sure it uses the correct backend
    
    * Adjust warning flags

--- Plugin/modules/WarningFlags.cmake
+++ Plugin/modules/WarningFlags.cmake
@@ -5,6 +5,7 @@ if((CMAKE_CXX_COMPILER_ID STREQUAL "MSVC") OR (CMAKE_CXX_SIMULATE_ID STREQUAL "M
         /W4     # base warning level
         /wd4458 # declaration hides class member (from Foley's GUI Magic)
         /wd4505 # since VS2019 doesn't handle [[ maybe_unused ]] for static functions (RTNeural::debug_print)
+        /wd4244 # for XSIMD
     )
 elseif((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") OR (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang"))
     target_compile_options(warning_flags INTERFACE
@@ -21,6 +22,9 @@ elseif((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") OR (CMAKE_CXX_COMPILER_ID STREQU
         # Comment them out to be more strict.
         -Wno-shadow-field-in-constructor -Wno-shadow-field -Wno-sign-conversion
         -Wno-float-conversion -Wno-implicit-float-conversion -Wno-switch-enum
+        # For XSIMD
+        -Wno-cast-align -Wno-shadow -Wno-implicit-int-conversion
+        -Wno-zero-as-null-pointer-constant
     )
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     target_compile_options(warning_flags INTERFACE
@@ -32,6 +36,8 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
         # These lines suppress some warnings from Foley's GUI Magic and RTNeural.
         # Comment them out to be more strict.
         -Wno-sign-conversion -Wno-switch-enum -Wno-overloaded-virtual
+        # For XSIMD
+        -Wno-zero-as-null-pointer-constant
         # From LV2 Wrapper
         -Wno-parentheses -Wno-deprecated-declarations -Wno-redundant-decls
     )
