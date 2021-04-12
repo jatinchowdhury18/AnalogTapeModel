6bcb619de71b81025ac1bfdfbd93a7ce14458a45
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-04-12 15:06:28 -0700

    Fix unit tests runner

--- Plugin/Source/Headless/UnitTests/UnitTests.h
+++ Plugin/Source/Headless/UnitTests/UnitTests.h
@@ -9,7 +9,7 @@ public:
 
 private:
     int64 getRandomSeed (const ArgumentList& args);
-    void getTestsForArgs (Array<UnitTest*>& tests, const ArgumentList& args);
+    Array<UnitTest*> getTestsForArgs (const ArgumentList& args);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (UnitTests)
 };
