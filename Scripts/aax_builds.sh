0bff95bd945c289ee48c4e3e66618f9ae97f5b5b
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-11-11 21:13:18 +0000

    Working AAX builds on Windows

--- Scripts/aax_builds.sh
+++ Scripts/aax_builds.sh
@@ -11,44 +11,86 @@ else
     build_config="Release"
 fi
 
-# clean up old builds
 cd Plugin
 
+# clean up old builds
 if [[ $* = *clean* ]]; then
     echo "Cleaning previous build..."
     rm -rf build-aax/
 fi
 
+# set up OS-dependent variables
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Building for MAC"
+
+    # Mac needs sudo mode for copying builds
+    sudo su -
+
+    AAX_PATH=~/Developer/AAX_SDK/
+    ilok_pass=$(more ~/Developer/ilok_pass)
+    aax_target_dir="/Library/Application\ Support/Avid/Audio/Plug-Ins/"
+    SED_FLAGS="-i ''"
+    TEAM_ID=$(more ~/Developer/mac_id)
+
+else # Windows
+    echo "Building for WINDOWS"
+
+    AAX_PATH=C:/SDKs/AAX_SDK/
+    ilok_pass=$(cat /d/ilok_pass)
+    aax_target_dir="/c/Program Files/Common Files/Avid/Audio/Plug-Ins/"
+    SED_FLAGS="-i -e"
+fi
+
 # set up build AAX
-AAX_PATH=~/Developer/AAX_SDK/
-sed -i '' "s~# juce_set_aax_sdk_path.*~juce_set_aax_sdk_path(${AAX_PATH})~" CMakeLists.txt
+sed $SED_FLAGS "s~# juce_set_aax_sdk_path.*~juce_set_aax_sdk_path(${AAX_PATH})~" CMakeLists.txt
 
 # cmake new builds
-TEAM_ID=$(more ~/Developer/mac_id)
-cmake -Bbuild-aax -GXcode -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY="Developer ID Application" \
-    -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM="$TEAM_ID" \
-    -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_STYLE="Manual" \
-    -D"CMAKE_OSX_ARCHITECTURES=x86_64" \
-    -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_INJECT_BASE_ENTITLEMENTS=NO \
-    -DCMAKE_XCODE_ATTRIBUTE_OTHER_CODE_SIGN_FLAGS="--timestamp" \
-    -DMACOS_RELEASE=ON
-cmake --build build-aax --config $build_config -j12 --target CHOWTapeModel_AAX | xcpretty
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    cmake -Bbuild-aax -GXcode -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY="Developer ID Application" \
+        -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM="$TEAM_ID" \
+        -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_STYLE="Manual" \
+        -D"CMAKE_OSX_ARCHITECTURES=x86_64" \
+        -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_INJECT_BASE_ENTITLEMENTS=NO \
+        -DCMAKE_XCODE_ATTRIBUTE_OTHER_CODE_SIGN_FLAGS="--timestamp" \
+        -DMACOS_RELEASE=ON
+
+    cmake --build build-aax --config $build_config --parallel $(nproc) --target CHOWTapeModel_AAX | xcpretty
+
+else # Windows
+    cmake -Bbuild-aax -G"Visual Studio 16 2019" -A x64
+    cmake --build build-aax --config $build_config --parallel $(nproc) --target CHOWTapeModel_AAX
+fi
 
 # sign with PACE
 aax_location=build-aax/CHOWTapeModel_artefacts/$build_config/AAX/CHOWTapeModel.aaxplugin
 wcguid="D3FA57E0-3DA5-11EC-8891-005056920FF7"
-ilok_pass=$(more ~/Developer/ilok_pass)
-wraptool sign --verbose \
-    --account chowdsp \
-    --password "$ilok_pass" \
-    --wcguid $wcguid \
-    --dsig1-compat off \
-    --signid "Developer ID Application: Jatin Chowdhury" \
-    --in $aax_location \
-    --out $aax_location
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    wraptool sign --verbose \
+        --account chowdsp \
+        --password "$ilok_pass" \
+        --wcguid $wcguid \
+        --dsig1-compat off \
+        --signid "Developer ID Application: Jatin Chowdhury" \
+        --in $aax_location \
+        --out $aax_location
+
+    wraptool verify --verbose --in $aax_location/Contents/MacOS/CHOWTapeModel
+
+else # Windows
+    wraptool sign --verbose \
+        --account chowdsp \
+        --password "$ilok_pass" \
+        --wcguid $wcguid \
+        --keyfile /c/Users/jatin/Downloads/jatin_aax_cert.p12 \
+        --keypassword "$ilok_pass" \
+        --in $aax_location \
+        --out $aax_location
+        
+    wraptool verify --verbose --in $aax_location/Contents/x64/CHOWTapeModel.aaxplugin
+fi
 
 # reset AAX SDK field...
-sed -i '' "s~juce_set_aax_sdk_path.*~# juce_set_aax_sdk_path(${AAX_PATH})~" CMakeLists.txt
+sed $SED_FLAGS "s~juce_set_aax_sdk_path.*~# juce_set_aax_sdk_path(NONE)~" CMakeLists.txt
 
-sudo rm -rf /Library/Application\ Support/Avid/Audio/Plug-Ins/CHOWTapeModel.aaxplugin
-sudo cp -R $aax_location /Library/Application\ Support/Avid/Audio/Plug-Ins/
+rm -rf "$aax_target_dir/CHOWTapeModel.aaxplugin"
+cp -R "$aax_location" "$aax_target_dir"
