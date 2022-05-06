251c04477c3a70e5192b5865863c7d8a79bc725b
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-05-06 22:26:30 +0100

    Set up separate 32-bit installer

--- Plugin/Installers/windows/build_win_installer.sh
+++ Plugin/Installers/windows/build_win_installer.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-script_file=ChowTapeModel_Install_Script.iss
-
-app_version=$(cut -f 2 -d '=' <<< "$(grep 'CMAKE_PROJECT_VERSION:STATIC' ../../build/CMakeCache.txt)")
+app_version=$(cut -f 2 -d '=' <<< "$(grep 'CMAKE_PROJECT_VERSION:STATIC' ../../build32/CMakeCache.txt)")
 echo "Setting app version: $app_version..."
+
+echo "Creating 64-bit installer..."
+script_file=ChowTapeModel_Install_Script.iss
 sed -i "s/##APPVERSION##/${app_version}/g" $script_file
-
-# build installer
-echo Building...
 $"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" $script_file
+sed -i "s/${app_version}/##APPVERSION##/g" $script_file # reset version number
 
-# reset version number
-sed -i "s/${app_version}/##APPVERSION##/g" $script_file
+echo "Creating 32-bit installer..."
+script_file=ChowTapeModel_Install_Script_32bit.iss
+sed -i "s/##APPVERSION##/${app_version}/g" $script_file
+$"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" $script_file
+sed -i "s/${app_version}/##APPVERSION##/g" $script_file # reset version number
 
 exec="ChowTapeModel-Win-$app_version.exe"
 direc=$PWD
--- Scripts/win_builds.sh
+++ Scripts/win_builds.sh
@@ -3,12 +3,12 @@
 cd Plugin
 
 build64(){
-    cmake -Bbuild -G"Visual Studio 15 2017 Win64"
+    cmake -Bbuild -G"Visual Studio 16 2019" -A x64
     cmake --build build --config Release -j4
 }
 
 build32(){
-    cmake -Bbuild32 -G"Visual Studio 15 2017"
+    cmake -Bbuild32 -G"Visual Studio 16 2019" -A Win32
     cmake --build build32 --config Release -j4
 }
 
