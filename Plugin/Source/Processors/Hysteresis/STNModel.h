3217a67734702b623db04e304ca573bed0a9ca80
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-05-15 22:28:06 +0100

    Try to fix unit test with compiler error

--- Plugin/Source/Processors/Hysteresis/STNModel.h
+++ Plugin/Source/Processors/Hysteresis/STNModel.h
@@ -1,10 +1,7 @@
 #pragma once
 
 #include <JuceHeader.h>
-
-JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wshorten-64-to-32")
 #include <RTNeural/RTNeural.h>
-JUCE_END_IGNORE_WARNINGS_GCC_LIKE
 
 #define USE_RTNEURAL_POLY 0
 #define USE_RTNEURAL_STATIC 1
