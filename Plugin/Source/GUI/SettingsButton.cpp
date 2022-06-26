d27e0b8b8a82b83e5b83d16e702d13b3372947aa
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2022-06-26 14:41:26 +0100

    Accessibility work (#274)
    * Tweak OpenGL settings to help with accessibility with NVDA
    
    * Update tab navigation for accessibility

--- Plugin/Source/GUI/SettingsButton.cpp
+++ Plugin/Source/GUI/SettingsButton.cpp
@@ -96,30 +96,5 @@ void SettingsButton::openGLManu (PopupMenu& menu, int itemID)
 void SettingsButton::copyDiagnosticInfo()
 {
     Logger::writeToLog ("Copying diagnostic info...");
-
-    String diagString;
-    diagString += "Version: " + proc.getName() + " " + String (JucePlugin_VersionString) + "\n";
-    diagString += "Commit: " + String (CHOWDSP_GIT_COMMIT_HASH) + " on " + String (CHOWDSP_GIT_BRANCH)
-                  + " with JUCE version " + SystemStats::getJUCEVersion() + "\n";
-
-    // build system info
-    diagString += "Build: " + Time::getCompilationDate().toString (true, true, false, true)
-                  + " on " + String (CHOWDSP_BUILD_FQDN)
-                  + " with " + String (CHOWDSP_CXX_COMPILER_ID) + "-" + String (CHOWDSP_CXX_COMPILER_VERSION) + "\n";
-
-    // user system info
-    diagString += "System: " + SystemStats::getDeviceDescription()
-                  + " with " + SystemStats::getOperatingSystemName()
-                  + (SystemStats::isOperatingSystem64Bit() ? " (64-bit)" : String())
-                  + (SystemStats::isRunningInAppExtensionSandbox() ? " (Sandboxed)" : String())
-                  + " on " + String (SystemStats::getNumCpus()) + " Core, " + SystemStats::getCpuModel() + "\n";
-
-    // plugin info
-    PluginHostType hostType {};
-    diagString += "Plugin Info: " + proc.getWrapperTypeString()
-                  + " running in " + String (hostType.getHostDescription())
-                  + " running at " + String (proc.getSampleRate() / 1000.0, 1) + " kHz "
-                  + "with block size: " + String (proc.getBlockSize()) + "\n";
-
-    SystemClipboard::copyTextToClipboard (diagString);
+    SystemClipboard::copyTextToClipboard (chowdsp::PluginDiagnosticInfo::getDiagnosticsString (proc));
 }
